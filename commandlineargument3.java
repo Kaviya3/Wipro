da6983cc4ab38cda547efe221e8343407e15b4d8
Kaviya3 <40487587+Kaviya3@users.noreply.github.com>
2018-06-26 15:46:47 +0530

    Create commandlineargument3.java

---
+++ commandlineargument3.java
@@ -0,0 +1,14 @@
+public class JavaApplication1 {
+
+  
+    public static void main(String[] args) {
+        System.out.println("Addition of two numbers");
+       
+         int a = Integer.parseInt(args[0]);
+          int b = Integer.parseInt(args[1]);
+          int sum=a+b;
+     System.out.println("sum=" +sum);
+    }
+    
+}
+
