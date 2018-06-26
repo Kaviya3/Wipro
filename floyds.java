1af01b5f04bfc6bbd903c5d4c34a98da235770a5
Kaviya3 <40487587+Kaviya3@users.noreply.github.com>
2018-06-26 14:58:27 +0530

    Create floyds.java

---
+++ floyds.java
@@ -0,0 +1,27 @@
+package javaapplication19;
+
+import java.util.Scanner;
+public class JavaApplication19 {
+    
+    public static void main(String[] args) {
+           int n, c, d;
+      Scanner in = new Scanner(System.in);
+ 
+      System.out.println("Enter the number of rows of floyd's triangle you want");
+      n = in.nextInt();
+ 
+      System.out.println("Floyd's triangle :-");
+ 
+      for ( c = 1 ; c <= n ; c++ )
+      {
+         for ( d = 1 ; d <= c ; d++ )
+         {
+            System.out.print("*");
+            
+         }
+ 
+         System.out.println();
+      }
+   }
+    
+}
