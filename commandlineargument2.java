53cc953f2aaead5b93eb5616af77b27c6e51f996
Kaviya3 <40487587+Kaviya3@users.noreply.github.com>
2018-06-26 15:45:52 +0530

    Create commandlineargument2.java

---
+++ commandlineargument2.java
@@ -0,0 +1,16 @@
+package javaapplication1;
+
+
+public class JavaApplication1 {
+
+  
+    public static void main(String[] args) {
+        System.out.println("welcome");
+        System.out.println("argument1:"+args[0]);
+        
+    
+    }
+    
+}
+
+
