753b60ae967660e7b165611226aa3a0fdbd8b61d
Kaviya3 <40487587+Kaviya3@users.noreply.github.com>
2018-06-26 14:15:03 +0530

    Create commandlineargument1.java

---
+++ commandlineargument1.java
@@ -0,0 +1,16 @@
+package javaapplication1;
+
+
+public class JavaApplication1 {
+
+  
+    public static void main(String[] args) {
+        
+        System.out.println("argument1:"+args[0]);
+        System.out.println("Technology");
+        System.out.println("argument2:"+args[1]);
+    }
+    
+}
+
+
