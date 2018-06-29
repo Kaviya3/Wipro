fa6c52510f4da94effe5d529e2152090d32c4053
Kaviya3 <40487587+Kaviya3@users.noreply.github.com>
2018-06-29 12:13:45 +0530

    Create replace.java

---
+++ replace.java
@@ -0,0 +1,11 @@
+package javaapplication35;
+
+public class JavaApplication35 {
+
+    public static void main(String[] args) {
+        String s="kavya";
+        String b=s.replace('a','e');
+    System.out.println(b);
+}
+    }
+
