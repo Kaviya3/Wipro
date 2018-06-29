474252ff61dcbe78879c7540763ad340be87ec7e
Kaviya3 <40487587+Kaviya3@users.noreply.github.com>
2018-06-29 09:47:10 +0530

    Create insertcharinstring.java

---
+++ insertcharinstring.java
@@ -0,0 +1,16 @@
+package javaapplication35;
+
+public class JavaApplication35 {
+
+    public static void main(String[] args) {
+     
+      StringBuilder sb=new StringBuilder("maniviji");
+  
+      sb.insert(4, 'b');
+      
+      System.out.print(sb.toString());
+      
+      
+    }
+    
+}
